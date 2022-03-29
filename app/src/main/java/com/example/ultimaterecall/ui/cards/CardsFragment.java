afc7ef7d6997f70f863a952d711bd2da1b32bb6c
Pr0gression <39017862+Pr0gression@users.noreply.github.com>
2022-03-29 10:39:37 -0500

    added "click detection"

--- app/src/main/java/com/example/ultimaterecall/ui/cards/CardsFragment.java
+++ app/src/main/java/com/example/ultimaterecall/ui/cards/CardsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -53,7 +54,17 @@ public class CardsFragment extends Fragment {
         mAdapter = new PackAdapter(PackObjectArray);
         mRecyclerView.setAdapter(mAdapter);
 
+        mRecyclerView.addOnItemTouchListener(new RecyclerViewTouchListener(getActivity().getApplicationContext(), mRecyclerView, new RecyclerViewClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                System.out.println("Hey there from " + position);
+            }
 
+            @Override
+            public void onLongClick(View view, int position) {
+                System.out.println("Hey there");
+            }
+        }));
         return root;
     }
 
