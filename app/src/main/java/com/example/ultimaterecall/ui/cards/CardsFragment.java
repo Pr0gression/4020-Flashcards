7337bf4439002825c39528a2c88e4810ba1b2749
Pr0gression <39017862+Pr0gression@users.noreply.github.com>
2022-03-29 10:24:20 -0500

    Removed ViewModel class

--- app/src/main/java/com/example/ultimaterecall/ui/cards/CardsFragment.java
+++ app/src/main/java/com/example/ultimaterecall/ui/cards/CardsFragment.java
@@ -42,8 +42,6 @@ public class CardsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        CardsViewModel cardsViewModel =
-                new ViewModelProvider(this).get(CardsViewModel.class);
 
         binding = FragmentCardsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
